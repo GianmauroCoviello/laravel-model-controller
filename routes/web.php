467cf5b838543d150b24f403b76b03e28bba8776
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-27 03:45:32 +0200

    inserita la rotta del controller e inserito il layouts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use app\http\Controllers\ControllerComics as ControllerComics;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ControllerComics::class, 'index']);
